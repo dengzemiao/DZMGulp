56c2b1d7ad1e2afa4492f4994283fcb7114019e3
dengzemiao <dengzemiaovip@163.com>
2024-03-25 16:08:59 +0800

    2024-03-25 16:07:59 提交优化

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ const ignorePaths = [
   // '/Users/dengzemiao/Desktop/GitHub/DZMLuckyDrawPro/js/index.js',
 
   // 压缩报错解决：下面这个文件在压缩时报错，遇到这种报错的且没有更好的解决方案，可以直接忽略拷贝过去使用，不要进行压缩就好了，或者修复掉代码内部导致报错的问题，再次发起压缩
-  // Error: GulpUglifyError: unable to minify JavaScript Caused by: SyntaxError: 'return' outside of function
+  // Error: GulpUglifyError: unable to minify JavaScript; Caused by: SyntaxError: 'return' outside of function; Line: 52; Col: 2;
   // '/Users/dengzemiao/Desktop/Project/script/vue_script/demo/luckydrawpro/lib/ant-design-vue/scripts/prettier.js'
 ]
 // 任务列表
